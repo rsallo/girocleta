befd8ec81ceba3ad2d8907c98333633422488176
Robert Sallo <rsallo@gmail.com>
2014-11-26 23:52:50 +0100

    Updated config

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,11 @@
 // include composer autoloader
 include_once __DIR__ .'/vendor/autoload.php';
 
+
+// include presets
+include_once __DIR__ .'/presets.php';
+
+
 // define the path to the config folder
 define('CONFIG_PATH', realpath(__DIR__ .'/app/shared/config'));
 
